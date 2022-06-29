b7325d7d3a1b8492bae95a0c71f3d543c30ccc72
Antony Konstantinidis <ak@digitalessence.de>
2022-06-29 15:30:49 +0200

    Create a MouseCursor component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <AppNavigation />
+  <MouseCursor />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import AppNavigation from "@/components/AppNavigation.vue";
+import MouseCursor from "@/components/MouseCursor.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     AppNavigation,
+    MouseCursor,
   },
 });
 </script>
