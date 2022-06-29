3a5637c9ca73b81dfb49d970900f98da0533adc4
Antony Konstantinidis <ak@digitalessence.de>
2022-06-29 16:00:51 +0200

    Use Lazy Loading

--- src/router.ts
+++ src/router.ts
@@ -3,11 +3,11 @@ import {
   createWebHistory,
   type RouteRecordRaw,
 } from "vue-router";
-import BookList from "@/views/BookList.vue";
 import NotFound from "@/views/NotFound.vue";
 import AboutPage from "@/views/AboutPage.vue";
-import BookDetail from "@/views/BookDetail.vue";
-import BooksPage from "@/views/BooksPage.vue";
+const BooksPage = () => import("@/views/BooksPage.vue");
+const BookDetail = () => import("@/views/BookDetail.vue");
+const BookList = () => import("@/views/BookList.vue");
 
 const routes: Array<RouteRecordRaw> = [
   {
