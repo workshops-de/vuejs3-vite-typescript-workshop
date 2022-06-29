9e275f5e3d74c413775c5f811d3c341ce02dd256
Antony Konstantinidis <ak@digitalessence.de>
2022-06-29 15:31:50 +0200

    Create an InfoBox component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <AppNavigation />
   <MouseCursor />
+  <InfoBox />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import AppNavigation from "@/components/AppNavigation.vue";
 import MouseCursor from "@/components/MouseCursor.vue";
+import InfoBox from "./components/InfoBox.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     AppNavigation,
     MouseCursor,
+    InfoBox,
   },
 });
 </script>
